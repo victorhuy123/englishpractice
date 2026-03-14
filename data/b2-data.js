9aa01f1a3d4aab88c67e2dd9202ae00f8c47d445
victorhuy123 <88974469+victorhuy123@users.noreply.github.com>
2026-03-14 22:23:15 +0700

    Create b2-data.js

---
+++ data/b2-data.js
@@ -0,0 +1,4 @@
+const B2_CONTENT = [
+    { type: "flashcard", vn: "Đồng ý hoàn toàn", en: "You took the words right out of my mouth!" },
+    { type: "mcq", q: "Khi muốn nói một kế hoạch rất khó xảy ra:", options: ["It's a long shot", "It's a far jump", "It's a long walk"], correct: 0, vn: "Cơ hội rất mong manh." }
+];
